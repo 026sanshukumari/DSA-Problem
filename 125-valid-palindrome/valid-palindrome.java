387ad188c685d809fb7da574819b30af7b87f6bb
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2025-10-14 12:28:20 +0530

    Time: 2 ms (98.62%) | Memory: 42.7 MB (98.19%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -6,19 +6,18 @@ class Solution {
         int start = 0, end = s.length() - 1;
         while(start <= end){
             while(start < end && !Character.isLetterOrDigit(s.charAt(start))){
-                start++;
-            }
-            while(start < end && !Character.isLetterOrDigit(s.charAt(end))){
-                end--;
-            }
-            if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))){
-                return false;
-            }
-            else{
-                start++;
-                end--;
-            } 
+            start++;
         }
-        return true;
+        while(start < end && !Character.isLetterOrDigit(s.charAt(end))){
+        end--;
     }
+    if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))){
+        return false;
+    }else{
+        start++;
+        end--;
+    }
+        }
+    return true;
+}
 }
