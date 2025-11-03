1589931a913bcdcc62bddc1b9903d247cc09ffab
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2025-11-03 21:07:33 +0530

    Time: 28 ms (91.80%) | Memory: 143.5 MB (5.43%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,21 +1,34 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        // if (nums == null || nums.length == 0 || k <= 0) return new int[0];
+        // int n = nums.length;
+        // if (k == 1) return nums.clone();
+
+        Deque<Integer> dq = new ArrayDeque<>();
         int n = nums.length;
-        int[] idx = new int[n-k+1];
-        int ri = 0;
-        Deque<Integer> q = new ArrayDeque<>();
-        for(int i=0; i<nums.length; i++){
-            if(!q.isEmpty() && q.peek() == i-k){
-                q.poll();
+        int[] res = new int[n-k+1];
+        int idx = 0;
+        //1st wind
+        for(int i=0; i<k; i++){
+            while(!dq.isEmpty() && nums[dq.getLast()] <= nums[i]){
+                dq.removeLast();
             }
-            while(!q.isEmpty() && nums[q.peekLast()] < nums[i]){
-                q.pollLast();
-            }
-            q.offer(i);
-            if(i >= k - 1){
-                idx[ri++] = nums[q.peek()];
-            }
+            dq.addLast(i);
         }
-        return idx;
+        for(int i=k; i<n; i++){
+            res[idx++] = nums[dq.getFirst()];
+
+            //remove elmet not part of curr wind
+            while(!dq.isEmpty() && dq.getFirst() <= i-k){
+                dq.removeFirst();
+            }
+            //remove the smaller vals
+              while(!dq.isEmpty() && nums[dq.getLast()] <= nums[i]){
+                dq.removeLast();
+            }
+            dq.addLast(i);
+        }
+        res[idx] = nums[dq.getFirst()];
+        return res;
     }
 }
