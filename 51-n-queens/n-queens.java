fa4c8f21a4bc20962582d9320e71a3262eda71bc
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2025-12-29 14:37:10 +0530

    Time: 2 ms (80.30%) | Memory: 46.7 MB (40.98%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,57 @@
+class Solution {
+
+    List<List<String>> result = new ArrayList<>();
+
+    public boolean isSafe(char[][] board, int row, int col) {
+        // check column
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') return false;
+        }
+
+        // check left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        // check right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        return true;
+    }
+
+    public void nQueens(char[][] board, int row) {
+        if (row == board.length) {
+            result.add(construct(board));
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                nQueens(board, row + 1);
+                board[row][col] = '.'; // backtrack
+            }
+        }
+    }
+
+    public List<String> construct(char[][] board) {
+        List<String> path = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            path.add(new String(board[i]));
+        }
+        return path;
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        nQueens(board, 0);
+        return result;
+    }
+}
