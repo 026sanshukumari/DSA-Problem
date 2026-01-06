2348205a63b1ac4c52a053d384818e55a84837fe
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-01-06 23:25:38 +0530

    Time: 3 ms (12.42%) | Memory: 44.4 MB (14.26%) - LeetSync

---
+++ 926-find-and-replace-pattern/find-and-replace-pattern.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> res = new ArrayList<>();
+        String normalizedpatt = normalize(pattern);
+        for(String word : words){
+            if(word.length() != pattern.length()){
+                continue;
+            }
+            if(normalize(word).equals(normalizedpatt)){
+                res.add(word);
+            }
+        }
+        return res;
+    }
+    private String normalize(String s){
+        HashMap<Character, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        int id = 0;
+
+        for(char c : s.toCharArray()){
+            if(!map.containsKey(c)){
+                map.put(c,id++);
+            }
+            sb.append(map.get(c)).append("#");
+        }
+        return sb.toString();
+    }
+}
