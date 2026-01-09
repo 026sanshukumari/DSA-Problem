3424ce444b594a62c24e45fb0f1193941e9c0233
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-01-09 23:30:53 +0530

    Time: 8 ms (81.17%) | Memory: 45.3 MB (34.57%) - LeetSync

---
+++ 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int balancedString(String s) {
+        int n = s.length();
+        int required = n / 4;
+        
+        int[] count = new int[128];
+        for (char c : s.toCharArray()) {
+            count[c]++;
+        }
+        if (count['Q'] == required &&
+            count['W'] == required &&
+            count['E'] == required &&
+            count['R'] == required) {
+            return 0;
+        }
+
+        int left = 0;
+        int ans = n;
+
+        for (int right = 0; right < n; right++) {
+            count[s.charAt(right)]--;
+
+            while (left < n &&
+                   count['Q'] <= required &&
+                   count['W'] <= required &&
+                   count['E'] <= required &&
+                   count['R'] <= required) {
+
+                ans = Math.min(ans, right - left + 1);
+                count[s.charAt(left)]++;
+                left++;
+            }
+        }
+
+        return ans;
+    }
+}
