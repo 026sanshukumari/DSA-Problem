690f6aeeed3fbc1d93d8d3227a01b5855c413794
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-01-14 23:54:48 +0530

    Time: 24 ms (78.42%) | Memory: 54.7 MB (51.73%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -3,6 +3,7 @@ class Solution {
         PriorityQueue<int[]> pq = new PriorityQueue<>(
             (a, b) -> a[0] - b[0]
         );
+
         for (int i = 0; i < points.length; i++) {
             int distSq = points[i][0] * points[i][0]
                        + points[i][1] * points[i][1];
