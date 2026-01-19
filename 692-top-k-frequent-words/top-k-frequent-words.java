d42fdb201014790bfb34ec0b54093fcb4a42cd5d
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-01-19 23:10:49 +0530

    Time: 7 ms (84.80%) | Memory: 46.4 MB (73.31%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -4,17 +4,17 @@ class Solution {
         for(String word : words){
             map.put(word,map.getOrDefault(word,0) + 1);
         }
-    PriorityQueue<String> maxHeap = new PriorityQueue<>((a,b) -> {
-    if(!map.get(a).equals(map.get(b)))
-        return map.get(b) - map.get(a);
-        return a.compareTo(b);
-    }
-    );
-    maxHeap.addAll(map.keySet());
-     List<String> res = new ArrayList<>();
-    for(int i=0; i<k; i++){
-        res.add(maxHeap.poll());
-    }
+        PriorityQueue<String> maxHeap = new PriorityQueue<>((a,b) ->{
+            if(!map.get(a).equals(map.get(b)))
+            return map.get(b) - map.get(a);
+            return a.compareTo(b);
+        }
+        );
+        maxHeap.addAll(map.keySet());
+        List<String> res = new ArrayList<>();
+        for(int i=0; i<k; i++){
+            res.add(maxHeap.poll());
+        }
         return res;
     }
 }
