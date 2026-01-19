08a295134f98a57f42e67d4745e926ba30e45711
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-01-19 21:31:43 +0530

    Time: 8 ms (57.73%) | Memory: 46.5 MB (73.31%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String word : words){
+            map.put(word,map.getOrDefault(word,0) + 1);
+        }
+    PriorityQueue<String> maxHeap = new PriorityQueue<>((a,b) -> {
+    if(!map.get(a).equals(map.get(b)))
+        return map.get(b) - map.get(a);
+        return a.compareTo(b);
+    }
+    );
+    maxHeap.addAll(map.keySet());
+     List<String> res = new ArrayList<>();
+    for(int i=0; i<k; i++){
+        res.add(maxHeap.poll());
+    }
+        return res;
+    }
+}
