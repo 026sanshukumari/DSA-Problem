e1228963de35dfc7bf3ad0ea31951a3be70070bd
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-02-04 21:22:42 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (71.12%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
         int left = 1, right = num;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        while(left <= right){
+            int mid = left + (right - left)/2;
             long sqr = (long) mid * mid;
-
-            if (sqr == num) return true;
-            else if (sqr > num)
-                right = mid - 1;
+            if(sqr == num) return true;
+            else if(sqr > num) 
+            right = mid - 1;
             else
-                left = mid + 1;
+            left = mid + 1;
         }
         return false;
     }
