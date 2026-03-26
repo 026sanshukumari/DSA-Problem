18f64db2aad4ff6a09477f1958fb752263b48e0f
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-03-26 22:51:55 +0530

    Time: 168 ms (5.17%) | Memory: 48 MB (6.08%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,61 +1,54 @@
 class Solution {
-    class Pair {
-        int first, second;
-        Pair(int f, int s){
-            first = f;
-            second = s;
-        }
-    }
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
-
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<=n; i++){
+        for(int i = 0; i <= n; i++) {
             adj.add(new ArrayList<>());
         }
 
-        for(int[] edge : edges){
+        for(int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
 
             adj.get(u).add(v);
             adj.get(v).add(u);
 
-            if(isCycle(n, adj)){
-                return edge;
+            if(isCycle(n, adj)) {
+                return edge; 
             }
         }
+
         return new int[0];
     }
 
-    private boolean isCycle(int n, ArrayList<ArrayList<Integer>> adj){
+    private boolean isCycle(int n, ArrayList<ArrayList<Integer>> adj) {
         boolean vis[] = new boolean[n+1];
 
-        for(int i=1; i<=n; i++){
-            if(!vis[i]){
+        for(int i = 1; i <= n; i++) {
+            if(!vis[i]) {
                 if(bfs(i, adj, vis)) return true;
             }
         }
         return false;
     }
 
-    private boolean bfs(int start, ArrayList<ArrayList<Integer>> adj, boolean vis[]){
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(start,-1));
+    private boolean bfs(int start, ArrayList<ArrayList<Integer>> adj, boolean vis[]) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{start, -1});
         vis[start] = true;
 
-        while(!q.isEmpty()){
-            Pair curr = q.poll();
-            int node = curr.first;
-            int parent = curr.second;
+        while(!q.isEmpty()) {
+            int[] curr = q.poll();
+            int node = curr[0];
+            int parent = curr[1];
 
-            for(int adjNode : adj.get(node)){
-                if(!vis[adjNode]){
-                    vis[adjNode] = true;
-                    q.add(new Pair(adjNode, node));
+            for(int nei : adj.get(node)) {
+                if(!vis[nei]) {
+                    vis[nei] = true;
+                    q.add(new int[]{nei, node});
                 }
-                else if(parent != adjNode){
-                    return true;
+                else if(nei != parent) {
+                    return true; 
                 }
             }
         }
