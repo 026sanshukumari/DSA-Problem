09f48f257abb01c5106bc544d8f582cb732a2fe0
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-04-05 22:35:29 +0530

    Time: 2 ms (85.33%) | Memory: 42.4 MB (88.37%) - LeetSync

---
+++ 1304-longest-happy-string/longest-happy-string.java
@@ -0,0 +1,52 @@
+class Solution {
+    class Pair implements Comparable<Pair> {
+        int count;
+        char ch;
+
+        Pair(int count, char ch) {
+            this.count = count;
+            this.ch = ch;
+        }
+        public int compareTo(Pair that) {
+            return that.count - this.count;
+        }
+    }
+
+    public String longestDiverseString(int a, int b, int c) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        if (a > 0) pq.offer(new Pair(a, 'a'));
+        if (b > 0) pq.offer(new Pair(b, 'b'));
+        if (c > 0) pq.offer(new Pair(c, 'c'));
+
+        StringBuilder res = new StringBuilder();
+
+        while (!pq.isEmpty()) {
+            Pair node = pq.poll();
+            int n = res.length();
+
+            // check last 2 chars
+            if (n >= 2 && res.charAt(n - 1) == node.ch && res.charAt(n - 2) == node.ch) {
+                if (pq.isEmpty()) break;
+
+                Pair sec = pq.poll();
+                res.append(sec.ch);
+                sec.count--;
+
+                if (sec.count > 0) {
+                    pq.offer(sec);
+                }
+
+                pq.offer(node); // push back first choice
+            } else {
+                res.append(node.ch);
+                node.count--;
+
+                if (node.count > 0) {
+                    pq.offer(node);
+                }
+            }
+        }
+        return res.toString();
+    }
+}
