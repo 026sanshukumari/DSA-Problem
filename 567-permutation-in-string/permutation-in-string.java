361d9282078a701e7d5e8f32969d4e065e5b9a93
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-08-10 19:29:49 +0530

    Time: 7 ms (69.22%) | Memory: 43.8 MB (85.52%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,27 +1,23 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int left = 0;
-        HashMap<Character,Integer> map = new HashMap<>();
-        HashMap<Character,Integer> pmap = new HashMap<>();
+       int[] freq = new int[26];
+       int[] window = new int[26];
 
-        for(int i=0; i<s1.length(); i++){
-            pmap.put(s1.charAt(i),pmap.getOrDefault(s1.charAt(i), 0) + 1);
-        }
-        for(int right=0; right<s2.length(); right++){
-            map.put(s2.charAt(right), map.getOrDefault(s2.charAt(right), 0) + 1);
-            if(right - left + 1 == s1.length()){
-                if(map.equals(pmap)){
-                    return true;
-                }
-                char ch = s2.charAt(left);
-                map.put(ch, map.get(ch) - 1);
-                if(map.get(ch) == 0){
-                    map.remove(ch);
-                }
+       for(int i=0; i<s1.length(); i++){
+        freq[s1.charAt(i) - 'a']++;
+       }
+       int left = 0;
+       for(int right=0; right<s2.length(); right++){
+        window[s2.charAt(right) - 'a']++;
+        if(right - left + 1 == s1.length()){
+            if(Arrays.equals(freq,window)){
+                return true;
+            }
+            window[s2.charAt(left) - 'a']--;
             left++;
         }
-        }
-        return false;
+       }
+       return false;
 
     }
 }
