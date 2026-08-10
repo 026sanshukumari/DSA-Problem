ebec503c775ad795120282ffcd225f9f2c45743b
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-08-10 21:22:03 +0530

    Time: 20 ms (37.75%) | Memory: 48 MB (9.56%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,34 +1,41 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(char c : t.toCharArray()){
-            map.put(c, map.getOrDefault(c,0)+1);
-        }
-        int l = 0, r = 0;
-        int cnt = 0;
-        int minLen = Integer.MAX_VALUE;
-        int start = -1;
-        while(r < s.length()){
-            char c = s.charAt(r);
+       HashMap<Character,Integer> map = new HashMap<>();
+       HashMap<Character,Integer> pmap = new HashMap<>();
+       for(int i=0; i<t.length(); i++){
+        pmap.put(t.charAt(i), pmap.getOrDefault(t.charAt(i), 0) + 1);
+       }
 
-            if(map.containsKey(c)){
-                map.put(c, map.get(c)-1);
-                if(map.get(c) >= 0) cnt++;
-            }
-            while(cnt == t.length()){
-                if(r-l+1 < minLen){
-                    minLen = r-l+1;
-                    start = l;
-                }
-                char left = s.charAt(l);
-                if(map.containsKey(left)){
-                    map.put(left, map.get(left)+1);
-                    if(map.get(left) > 0) cnt--;
-                }
-                l++;
-            }
-            r++;
+       int formed = 0;
+       int required = pmap.size();
+       int left = 0;
+       int start = 0;
+       int minLen = Integer.MAX_VALUE;
+       for(int right = 0; right < s.length(); right++){
+        char ch = s.charAt(right);
+        map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+        if(pmap.containsKey(ch) && map.get(ch).equals(pmap.get(ch))){
+            formed++;
         }
-        return start == -1 ? "" : s.substring(start, start+minLen);
+
+        while(formed == required){
+            if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+            char chleft = s.charAt(left);
+            map.put(chleft,map.get(chleft) - 1);
+            if(pmap.containsKey(chleft) && map.get(chleft) < pmap.get(chleft)){
+                formed--;
+            }
+            left++;
+        }
+       }
+       if(minLen == Integer.MAX_VALUE){
+        return "";
+       }
+       return s.substring(start , start+minLen);
     }
 }
