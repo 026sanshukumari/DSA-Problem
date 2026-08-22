0409a551bb2f99c128c77892d35c7b3f6887b206
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-08-22 17:21:49 +0530

    Time: 6 ms (16.30%) | Memory: 175.6 MB (28.70%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,15 +1,18 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
+        int evenPos = 0;
+        int oddPos = 1;
         int[] ans = new int[nums.length];
-        int EvenPos = 0;
-        int OddPos = 1;
         for(int i=0; i<nums.length; i++){
             if(nums[i] > 0){
-                ans[EvenPos] = nums[i];
-                EvenPos += 2;
-            }else{
-                ans[OddPos] = nums[i];
-                OddPos += 2;
+                ans[evenPos] = nums[i];
+                evenPos += 2;
+            }
+        }
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] < 0){
+                ans[oddPos] = nums[i];
+                oddPos += 2;
             }
         }
         return ans;
