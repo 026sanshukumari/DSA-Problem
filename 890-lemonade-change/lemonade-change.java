cca1d64d1cd4c652037e949517ab648058a218a1
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-08-23 23:49:41 +0530

    Time: 2 ms (98.00%) | Memory: 72.4 MB (49.28%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -3,24 +3,25 @@ class Solution {
         int five = 0;
         int ten = 0;
         for(int i=0; i<bills.length; i++){
-            if(bills[i] == 5)
-            five++;
-            else if(bills[i] == 10){
-            if(five == 0) return false;
-            five--;
-            ten++;
-            }
-            else if(bills[i] == 20){
-                if(ten > 0 && five > 0){
-                    ten--;
+            if(bills[i] == 5){
+                five++;
+            }else if(bills[i] == 10){
+                if(five > 0){
                     five--;
-                }
-               else if(five >= 3){
-                    five -= 3;
-                }
-                else{
+                    ten++;
+                }else{
                     return false;
                 }
+                }else if(bills[i] == 20){
+                    if(five > 0 && ten > 0){
+                        five--;
+                        ten--;
+                    }else if(five >= 3){
+                    five -= 3;
+                }else{
+                    return false;
+                }
+                
             }
         }
         return true;
