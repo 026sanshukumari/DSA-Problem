b45ce9f61f90c588e4c7ade4080673c1c776022c
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-09-12 13:33:56 +0530

    Time: 383 ms (5.13%) | Memory: 47.5 MB (19.27%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,20 +1,32 @@
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(char ch : s.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0) + 1);
+        int[] freq = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i)]++;
         }
-        PriorityQueue<Character> pq = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
-          pq.addAll(map.keySet());
-        StringBuilder sb = new StringBuilder();
-        while(!pq.isEmpty()){
-            char ch = pq.poll();
-            int freq = map.get(ch);
-            for(int i=0; i<freq; i++){
-                sb.append(ch);
+
+        int maxFreq = 0;
+        for (int i = 0; i < freq.length; i++) {
+            maxFreq = Math.max(maxFreq, freq[i]);
+        }
+
+        String ans = "";
+
+        for (int f = maxFreq; f >= 1; f--) {
+
+            for (int i = 0; i < freq.length; i++) {
+
+                if (freq[i] == f) {
+                    char ch = (char) i;
+
+                    for (int j = 0; j < f; j++) {
+                        ans += ch;
+                    }
+                }
             }
         }
-        return sb.toString();
-        
+
+        return ans;
     }
 }
