5f6aded64a09b3b46ae6bd35feb21e1378352a61
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-09-15 01:22:14 +0530

    Time: 11 ms (66.04%) | Memory: 46.8 MB (57.55%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,23 +1,19 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> ans = new ArrayList<>();
-        int left = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        HashMap<Character,Integer> pmap = new HashMap<>();
-        for(int i=0; i<p.length(); i++){
-            pmap.put(p.charAt(i), pmap.getOrDefault(p.charAt(i), 0) + 1);
+        int[] freq = new int[26];
+        int[] freqWin = new int[26];
+        for(int i=0; i < p.length(); i++){
+            freq[p.charAt(i) - 'a']++;
         }
-        for(int right = 0; right<s.length(); right++){
-            map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
-
+        int left = 0;
+        for(int right = 0; right < s.length(); right++){
+            freqWin[s.charAt(right) - 'a']++;
             if(right - left + 1 == p.length()){
-                if(map.equals(pmap)){
+                if(Arrays.equals(freqWin, freq)){
                     ans.add(left);
                 }
-                map.put(s.charAt(left),map.get(s.charAt(left)) - 1);
-                if(map.get(s.charAt(left)) == 0){
-                    map.remove(s.charAt(left));
-                }
+                freqWin[s.charAt(left) - 'a']--;
                 left++;
             }
         }
