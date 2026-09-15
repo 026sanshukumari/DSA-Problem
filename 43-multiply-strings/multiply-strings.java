4f275e0b1cab43a92838567f4f0e270c02602bab
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-09-15 15:26:57 +0530

    Time: 3 ms (77.35%) | Memory: 43.8 MB (16.71%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,18 +1,31 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        int m = num1.length(), n = num2.length();
-        int[] pos = new int[m + n];
-        for(int i=m-1; i>=0; i--){
-            for(int j=n-1; j>=0; j--){
-                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                int p1 = i + j, p2 = i + j + 1;
-                int sum = mul + pos[p2];
-                pos[p1] += sum / 10;
-                pos[p2] = (sum) % 10;
-            }
+        if(num1.equals("0") || num2.equals("0")){
+            return "0";
         }
-        StringBuilder sb = new StringBuilder();
-        for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
-        return sb.length() == 0 ? "0" : sb.toString();
+        int[] res = new int[num1.length() + num2.length()];
+        int i = num1.length() - 1;
+        while(i >= 0){
+            int j = num2.length() - 1;
+            while(j >= 0){
+                int a = num1.charAt(i) - '0';
+                int b = num2.charAt(j) - '0';
+                int sum = a * b + res[i + j + 1];
+                int carry = sum / 10;
+                int digit = sum % 10;
+                res[i + j + 1] = digit;
+                res[i + j] += carry;
+                j--;
+            }
+            i--;
+        }
+        StringBuilder ans = new StringBuilder();
+        for(int num : res){
+            if(ans.length() == 0 && num == 0){
+               continue;
+            }
+             ans.append(num);
+        }
+        return ans.toString();
     }
 }
