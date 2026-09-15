53fe3e24168e99558601eb14635723a22434dbd8
026sanshukumari <161269960+026sanshukumari@users.noreply.github.com>
2026-09-15 14:47:12 +0530

    Time: 7 ms (71.71%) | Memory: 49.9 MB (26.23%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,27 +1,12 @@
-import java.util.*;
-
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs == null || strs.length == 0)
-            return new ArrayList<>();
-
-        Map<String, List<String>> map = new HashMap<>();
-        for (String str : strs) {
-            String key = getFreqStr(str);  // create signature for each string
-
-            if (map.containsKey(key)) {
-                map.get(key).add(str);
-            } else {
-                List<String> strList = new ArrayList<>();
-                strList.add(str);
-                map.put(key, strList);
-            }
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String word : strs){
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(word);
         }
         return new ArrayList<>(map.values());
-    }
-    private String getFreqStr(String str) {
-        char[] arr = str.toCharArray();
-        Arrays.sort(arr);  // sort characters to form key
-        return new String(arr);
     }
 }
